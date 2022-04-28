b9556ee544b891c1f14b781da610f8d2781c79af
yentlc <98667887+yentlc@users.noreply.github.com>
2022-04-28 14:49:21 -0300

    adding people arr

---
+++ app.js
@@ -0,0 +1,29 @@
+let people = [
+  {
+    img: '/images/person-1.jpg',
+    name: 'Jhon Human',
+    work: 'PROFESSIONAL REVIEWER',
+    review:
+      "I love reviewing stuff, I'm always on yelp reviewing restaurants, I always give bad reviews if they don't pick up my fork from the floor when I purposly throw it... If the michelin star restaurants do it why a domino's can't?",
+  },
+  {
+    img: '/images/person-2.jpg',
+    name: 'Jean Horse',
+    work: 'FUNERAL CRASHER',
+    review:
+      'Some people like to hire people like me to crash at their funerals and wreck havoc.',
+  },
+  {
+    img: '/images/person-3.jpg',
+    name: 'Sarah Notconnor',
+    work: 'CAT PSYCOLOGIST',
+    review:
+      "Approaching the state of mind of a cat It's complicated, some cats don't like to respond and they always fall asleep in the middle of a sesion",
+  },
+  {
+    img: '/images/person-4.jpg',
+    name: 'Leia Pricess',
+    work: 'JEDI ADVOCATE',
+    review: 'I provide support for the people who fight against the dark side',
+  },
+];
