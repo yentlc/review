e83549f4363fdcd0e371a4c36669e2cbebc45103
yentlc <98667887+yentlc@users.noreply.github.com>
2022-04-28 16:29:15 -0300

    almost finished

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let people = [
   {
-    img: '/images/person-1.jpg',
+    img: '/images/person-2.jpg',
     name: 'Jhon Human',
     work: 'PROFESSIONAL REVIEWER',
     review:
-      "I love reviewing stuff, I'm always on yelp reviewing restaurants, I always give bad reviews if they don't pick up my fork from the floor when I purposly throw it... If the michelin star restaurants do it why a domino's can't?",
+      "I love reviewing stuff, I'm always on yelp reviewing restaurants, I always give bad reviews if they don't pick up my fork from the floor when I purposly throw it... If the michelin star restaurants do it why domino's can't?",
   },
   {
-    img: '/images/person-2.jpg',
+    img: '/images/person-1.jpeg',
     name: 'Jean Horse',
     work: 'FUNERAL CRASHER',
     review:
@@ -27,3 +27,38 @@ let people = [
     review: 'I provide support for the people who fight against the dark side',
   },
 ];
+const leftBtn = document.querySelector('#left');
+const rightBtn = document.querySelector('#right');
+let img = document.querySelector('#profile-picture');
+let name = document.querySelector('#name');
+let work = document.querySelector('#work');
+let review = document.querySelector('#review');
+
+let count = 0;
+let current = people.length - 1;
+
+leftBtn.addEventListener('click', () => {
+  console.log(current);
+  if (current < 0) {
+    console.log(current);
+    current = people.length - 1;
+    console.log('despues de reasignar', current);
+  }
+  img.setAttribute('src', people[current].img);
+  name.innerText = people[current].name;
+  work.innerText = people[current].work;
+  review.innerText = people[current].review;
+  current--;
+});
+
+rightBtn.addEventListener('click', () => {
+  if (count === people.length) {
+    count = 0;
+  }
+  img.setAttribute('src', people[count].img);
+  name.innerText = people[count].name;
+  work.innerText = people[count].work;
+  review.innerText = people[count].review;
+  console.log(count);
+  count++;
+});
